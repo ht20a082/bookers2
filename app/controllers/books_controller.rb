7ac080fb4928a13244ed43c2683f48386b53a409
KeitoH <ht20a082@oecu.jp>
2022-07-31 03:27:16 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,50 @@
 class BooksController < ApplicationController
-  def index
+  def new
+    @book = Book.new
   end
 
-  def new
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @books = Book.all
   end
 
   def show
+    @Book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    flash[:notice] = "successfully"
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
+
+   private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
