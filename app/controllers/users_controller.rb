9952404a85d9c1315eb6211af466a813d587dbbd
KeitoH <ht20a082@oecu.jp>
2022-08-06 04:20:58 +0000

    Fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    #@books = Book.find(params[:id])
     @book = Book.new
     @books = @user.books
   end
