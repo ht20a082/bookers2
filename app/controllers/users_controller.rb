af827d6af825a4cd9c16afc644db5a03e9a2c6bb
KeitoH <ht20a082@oecu.jp>
2022-08-06 08:48:12 +0000

    Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    #@books = Book.find(params[:id])
     @book = Book.new
     @books = @user.books
   end
